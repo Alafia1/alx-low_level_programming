06faea6c34e0ccfe7136d088b1c591471c651759
Ismaila Muhammed <mdmhab@gmail.com>
2022-10-13 08:28:49 -0700

    here we godsk

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
+#include <unistd.h>
 
 /**
  * main - printing to the standard error
- * return: 1 is the new return
+ * Return: 1 is the new return
  */
+
 int main(void)
 {
-	fprintf(stderr, "and that piece of art is useful\" \- Dora Korpar, 2015\-10\-19\n");
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\m", 59);
 	return (1);
 }
