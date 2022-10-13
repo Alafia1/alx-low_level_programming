c08cd0e98b7650290e4598e28028e2a5287ea504
Ismaila Muhammed <mdmhab@gmail.com>
2022-10-13 06:39:17 -0700

    puts print

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,18 @@
 #include <stdio.h>
 
+/**
+ * main - prints th ephrase in the puts
+ * Return: 0 if exited properly, non-zero otherwise
+ */
+
 int main(void)
 {
+	/**
+	 * The main function is so cute
+	 *
+	 * Description: Printing the file with puts
+	 *
+	 */
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
