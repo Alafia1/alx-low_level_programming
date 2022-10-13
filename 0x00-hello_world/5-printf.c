4c3acbb89e06719ed2ba03b29552616d7daf8bb3
Ismaila Muhammed <mdmhab@gmail.com>
2022-10-13 06:55:54 -0700

    print f

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints using printf function
+ * Return: 0 if exited properly; non-zero otherwise
+ */
+int main(void)
+{
+	char Mystr[];
+
+	Mystr = "with proper grammar, but the outcome is a piece of art,";
+	printf(Mystr);
+	return (0);
+}
