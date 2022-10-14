bebd63f853669c633bf16ef51a05aefb329d90d9
Ismaila Muhammed <mdmhab@gmail.com>
2022-10-14 06:52:10 -0700

    here we go

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - this will print alphabet in lowercase
+ * Return: 0 if the program fails and non zero otherwise
+ */
+
+int main(void)
+{
+	char abc[26];
+	int i;
+
+	abc = "abcdefghijklmnopqrstuvwxyz";
+	for(i = 0;i < 26;i++)
+		putchar(abc[i]);
+	return (0);
+}
