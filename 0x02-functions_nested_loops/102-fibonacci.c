49cde68ed92537e9eda646867d2877bdb4f56f2e
Alafia1 <101114171+Alafia1@users.noreply.github.com>
2022-10-18 15:19:28 +0100

    fibonaci

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,30 +1,33 @@
-#include "holberton.h"
 #include <stdio.h>
+
 /**
-  * main - fibbonacci
-  *
-  *
-  * Return: 0
-  */
+ * main - main block
+ * Description: print the first 50 fibonacci numbers, starting with 1 and 2
+ *
+ * Return: 0
+*/
 int main(void)
 {
-long int a, b, c;
-long int i;
-int upto;
-a = 0;
-b = 1;
-c = 1;
-upto = 50;
-for (i = 1; i <= upto; i++)
-{
+	int count = 2;
+	long int i = 1, j = 2;
+	long int k;
 
-	printf("%li", c);
-	a = b;
-	b = c;
-	c = a + b;
-if (i < 50)
-	printf(", ");
-}
-printf("\n");
-return (0);
+	printf("%lu, ", i);
+	while (count <= 50)
+	{
+		if (count == 50)
+		{
+			printf("%lu\n", j);
+		}
+		else
+		{
+			printf("%lu, ", j);
+		}
+		k = j;
+		j += i;
+		i = k;
+		count++;
+	}
+
+	return (0);
 }
