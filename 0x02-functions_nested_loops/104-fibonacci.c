f8b0678527e2c3d0e4d742564b2f8379f578f4e6
Alafia1 <101114171+Alafia1@users.noreply.github.com>
2022-10-18 15:20:51 +0100

    fibonaci finish

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,47 +1,43 @@
-#include "holberton.h"
 #include <stdio.h>
+
 /**
-  * main - prints first 98 fibbonacci nums
-  *
-  *
-  * Return: 0
-  */
+ * main - prints the first 97 Fibonacci
+ * Decription: using main function
+ *
+ * Return: 0
+*/
 int main(void)
 {
-	/* declarations */
-	int i = 3;
-	int large = 0;
-	unsigned long a = 1, b = 2, term_1 = 0, a_2 = 0, b_2 = 0, term_2 = 0;
-	unsigned long cut = 10000000000000000;
-	/*printing the first nums */
-	printf("%lu, %lu", a, b);
-	/* looping up to 98 */
-	for (; i < 99; i++)
+	unsigned long int i, j, k, j1, j2, k1, k2;
+
+	j = 1;
+	k = 2;
+
+	printf("%lu", j);
+
+	for (i = 1; i < 91; i++)
 	{
-		/* prints comma space */
-		printf(", ");
-		/* dissecting large nums */
-		term_1 = a + b;
-		if (term_1 > cut)
-			large = 1;
-		/*printing large nums */
-		if (large)
-		{
-			term_2 = term_1 / cut + a_2 + b_2;
-			term_1 = term_1 % cut;
-			printf("%lu", term_2);
-			printf("%016lu", term_1);
-			a_2 = b_2;
-			b_2 = term_2;
-		}
-		/*printing regular nums */
-		else
-		{
-			printf("%lu", term_1);
-		}
-		a = b;
-		b = term_1;
+		printf(", %lu", k);
+		k = k + j;
+		j = k - j;
 	}
+
+	j1 = j / 1000000000;
+	j2 = j % 1000000000;
+	k1 = k / 1000000000;
+	k2 = k % 1000000000;
+
+	for (i = 92; i < 99; ++i)
+	{
+		printf(", %lu", k1 + (k2 / 1000000000));
+		printf("%lu", k2 % 1000000000);
+		k1 = k1 + j1;
+		j1 = k1 - j1;
+		k2 = k2 + j2;
+		j2 = k2 - j2;
+	}
+
 	printf("\n");
+
 	return (0);
 }
